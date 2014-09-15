1e1247702cf220307d079b735b42c357e80f4c31
Shaun Ellis <shaun@sdellis.com>
2014-09-15 14:46:10 -0400

    added template pre-process to display loris-base image links

--- sites/all/themes/exhibit_bootstrap/template.php
+++ sites/all/themes/exhibit_bootstrap/template.php
@@ -4,3 +4,10 @@
  * @file
  * template.php
  */
+
+function exhibit_bootstrap_preprocess_link(&$variables) {
+  if (strpos($variables['path'], 'loris') !== FALSE){
+  	$variables['text'] = '<img src="' . $variables['path'] . '" alt="' . $variables['text'] . '" title="' . $variables['text'] . '"/>';
+  }
+
+}
