c70f1a410dd80752f1cb47354bcba966a527cb00
Shaun Ellis <shaun@sdellis.com>
2014-09-24 10:26:21 -0400

    styling loris images

--- sites/all/themes/exhibit_bootstrap/template.php
+++ sites/all/themes/exhibit_bootstrap/template.php
@@ -7,7 +7,7 @@
 
 function exhibit_bootstrap_preprocess_link(&$variables) {
   if (strpos($variables['path'], 'loris') !== FALSE){
-  	$variables['text'] = '<img src="' . $variables['path'] . '" alt="' . $variables['text'] . '" title="' . $variables['text'] . '"/>';
+  	$variables['text'] = '<img class="thumbnail" src="' . $variables['path'] . '" alt="' . $variables['text'] . '" title="' . $variables['text'] . '"/>';
   }
 
 }
