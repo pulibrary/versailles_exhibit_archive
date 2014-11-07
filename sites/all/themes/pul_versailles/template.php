410cc4294c44607ac94e15c8c776d1970dffff16
Shaun Ellis <shaun@sdellis.com>
2014-11-07 13:51:10 -0500

    added back loris image links

--- sites/all/themes/pul_versailles/template.php
+++ sites/all/themes/pul_versailles/template.php
@@ -6,7 +6,6 @@
  * PUL Versailles theme.
  */
 
-
 function pul_versailles_preprocess_link(&$variables) {
   if (strpos($variables['path'], 'loris') !== FALSE){
   	$variables['text'] = '<img class="thumbnail" src="' . $variables['path'] . '" alt="' . $variables['text'] . '" title="' . $variables['text'] . '"/>';
