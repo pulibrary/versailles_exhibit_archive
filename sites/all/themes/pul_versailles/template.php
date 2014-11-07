9581fccdaa84a56b35a84150c7ca824eb33d85ef
Shaun Ellis <shaun@sdellis.com>
2014-11-07 13:31:06 -0500

    adding hero image

--- sites/all/themes/pul_versailles/template.php
+++ sites/all/themes/pul_versailles/template.php
@@ -5,3 +5,16 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * PUL Versailles theme.
  */
+
+
+function pul_versailles_preprocess_link(&$variables) {
+  if (strpos($variables['path'], 'loris') !== FALSE){
+  	$variables['text'] = '<img class="thumbnail" src="' . $variables['path'] . '" alt="' . $variables['text'] . '" title="' . $variables['text'] . '"/>';
+  	$p = str_replace("http://libimages.princeton.edu/loris/", "http://diglib.princeton.edu/tools/ib/", $variables['path']);
+  	$pos = strpos($variables['path'], 'full/');
+  	$p = substr($p, 0, $pos);
+  	$rpos = strrpos($p, '%2F', 0);
+  	$variables['path'] = urldecode(substr($p, 0, $rpos));
+  }
+
+}
